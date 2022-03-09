4271fb0a05b5eb95dbe31b4b1f3f5a440a3339d0
Katie Reider <ktreider5@gmail.com>
2022-03-09 16:21:51 -0500

    working on implementing gallery

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css';
 function HeroSection(props) {
     return (
         <div className='hero-container'>
-            <img src={props.image} alt="engagement"></img>
+            <img className='img' src={props.image} alt="engagement"></img>
             <h1 style={props.style}>{props.text}</h1>
         </div>
     )
---
+++ src/components/photos.js
@@ -0,0 +1,69 @@
+export const photos = [
+    {
+      src: require("../images/engagementphotos/Katie1665.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie1671.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie1708.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie1709.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie1777.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie2022.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie1889.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie2018.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie1781.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie2076.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    },
+    {
+      src: require("../images/engagementphotos/Katie2021.jpg"),
+      width: 3,
+      height: 4,
+      title: "Photo taken by: Yajaira Perez Photography"
+    }
+  ];
+  
