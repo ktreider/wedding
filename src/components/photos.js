8c434e955abc95b879bcf05f341bc9d08534b16a
Katie Reider <ktreider5@gmail.com>
2022-03-17 12:37:21 -0400

    all of the photosss

--- src/components/photos.js
+++ src/components/photos.js
@@ -1,5 +1,107 @@
 export const photos = [
     {
+      src: require("../images/k_j/firstHike.JPG"),
+      width: 3,
+      height: 4,
+      title: "Our first hike and photo at Pulpit Rock"
+    },
+    {
+      src: require("../images/k_j/ricketts1.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Ricketts Glen"
+    },
+    {
+      src: require("../images/k_j/ricketts2.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Ricketts Glen"
+    },
+    {
+      src: require("../images/k_j/delGap.HEIC"),
+      width: 3,
+      height: 4,
+      title: "The Delaware Water Gap. Mount Minsi"
+    },
+    {
+      src: require("../images/k_j/bakeOven.jpeg"),
+      width: 3,
+      height: 4,
+      title: "Bake Oven Knob"
+    },
+    {
+      src: require("../images/k_j/stargazing.jpeg"),
+      width: 3,
+      height: 4,
+      title: "Stargazing"
+    },
+    {
+      src: require("../images/k_j/hawkRock.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Hiking at Hawk Rock"
+    },
+    {
+      src: require("../images/k_j/iceHike.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Ice hiking at Ricketts Glen with Valley to Summit"
+    },
+    {
+      src: require("../images/k_j/snowboarding.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Joseph teaching Katie to snowboard"
+    },
+    {
+      src: require("../images/k_j/garKnots.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Homemade garlic knots"
+    },
+    {
+      src: require("../images/k_j/boulders.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Roxanne but its just Rocks. -Boulder Field"
+    },
+    {
+      src: require("../images/k_j/greenLane.jpeg"),
+      width: 3,
+      height: 4,
+      title: "Green Lane"
+    },
+    {
+      src: require("../images/k_j/lehighGap.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Lehigh Gap (the otherside)"
+    },
+    {
+      src: require("../images/k_j/grilledCheese.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Homemade grilled cheese and tomato soup"
+    },
+    {
+      src: require("../images/k_j/cherrySprings.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Stargazing at Cherry Springs"
+    },
+    {
+      src: require("../images/k_j/sumHike.jpg"),
+      width: 3,
+      height: 4,
+      title: "Hiking at Pole Steeple"
+    },
+    {
+      src: require("../images/k_j/ricketts1.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Ricketts Glen"
+    },
+    {
       src: require("../images/engagementphotos/Katie1665.jpg"),
       width: 3,
       height: 4,
@@ -64,6 +166,36 @@ export const photos = [
       width: 3,
       height: 4,
       title: "Photo taken by: Yajaira Perez Photography"
-    }
+    },
+    {
+      src: require("../images/k_j/ringingRocks.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Ringing Rocks"
+    },
+    {
+      src: require("../images/k_j/gnocchi.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Homemade Gnocchi"
+    },
+    {
+      src: require("../images/k_j/pumpkSeeds.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Using our leftover pumpkin seeds"
+    },
+    {
+      src: require("../images/k_j/starWars.jpg"),
+      width: 3,
+      height: 4,
+      title: "Katie being silly at Green Lane"
+    },
+    {
+      src: require("../images/k_j/mystery.HEIC"),
+      width: 3,
+      height: 4,
+      title: "Joseph at Green Lane"
+    },
   ];
   
