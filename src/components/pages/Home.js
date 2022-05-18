5ef1d6461943338f0c6dc95a5ab03fa088c91460
Katie Reider <ktreider5@gmail.com>
2022-05-18 14:23:25 -0400

    home page. schedule and locations

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
 
           <div className="mid-img">
           <img src={midImg} alt='Katie and Joseph'/>
-          <h6>Photos taken by the lovely: <a href="https://www.yajairaperez.com/" rel="noreferrer" target="_blank">Yajaira Perez Photography</a></h6>
+          <h6>Photos taken by the lovely: <a href="https://www.yajairaperez.com/" className="u" rel="noreferrer" target="_blank">Yajaira Perez Photography</a></h6>
           </div> {/*mid-img*/}
 
           <div className="right-side">
@@ -38,9 +38,57 @@ const Home = () => {
           <div id="button">
             <Button buttonStyle='btn--outline'>RSVP through The Knot here</Button>
           </div>
-
-          
       </div> {/*rsvp*/}
+
+      <div className="banner">
+          <div className="title">
+            <h1>Ceremony & Reception</h1>
+            <h2> October 1st, 2022</h2>
+          </div> 
+      </div> {/*banner*/}
+
+      <div className="schedule">
+        <div className="sched-info">
+          <h3>Schedule</h3>
+          <br></br>
+
+          <h4>Friday, September 30th, 2022</h4>
+          <p>Rehearsal & Dinner (wedding party)............................ 4:00 pm</p>
+          <br></br>
+
+          <h4>Saturday, October 1st, 2022</h4>
+          <p>Ceremony ............................................................................. 1:00 pm</p>
+          <p>Cocktail Hour ....................................................................... 5:00 pm</p>
+          <p>Dinner ..................................................................................... 6:30 pm</p>
+        </div> {/*sched-info*/}
+      </div> {/*schedule*/}
+
+
+      <div className="locations">
+          <div className="flex-styling">
+
+            <div className="loc ">
+              <div>
+                <h3>St Mary Catholic Church</h3>
+                <p>The ceremony will be held at the beautiful St. Mary’s Church in Conshohocken, PA. It was founded in 1905 as a Polish Parish and is now under the pastoral care of the Priestly Fraternity of St Peter. \nThe church is 25 minutes from the Philly Airport. </p>
+                <a href="https://goo.gl/maps/vevFoeRrKYgLf6iT7" className="u" rel="noreferrer" target="_blank">140 W Hector Street, Conshohocken, PA</a>
+              </div>
+              
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3053.066046161117!2d-75.30993858929499!3d40.073938849874054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6be43733fa251%3A0x437e07142ff275a!2s140%20W%20Hector%20St%2C%20Conshohocken%2C%20PA%2019428!5e0!3m2!1sen!2sus!4v1652896538695!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="st marys"></iframe>
+            </div> {/*loc*/}
+
+            <div className="loc right">
+              <div>
+                <h3>The Trolley Barn</h3>
+                <p>The Trolley Barn was built in 1896 as the terminal of the Quakertown-Richlandtown Traction Co., the building housed the region’s first electric trolleys, which ran from Main and Broad Streets to nearby Richlandtown. The Trolley Barn was renovated in late 2020 and now serves as a little public market and event space.</p>
+                <a href="https://goo.gl/maps/6BmbQtisx7jhzVRd7" className="u" rel="noreferrer" target="_blank">116 E Broad Street, Quakertown, PA</a>
+              </div>
+                
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3036.54695903798!2d-75.3383234695933!3d40.44102959608636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c418a26cd4749f%3A0x5a21bd6df6a53212!2s116%20E%20Broad%20St%2C%20Quakertown%2C%20PA%2018951!5e0!3m2!1sen!2sus!4v1652897372426!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="trolley barn"></iframe>            
+              </div> {/*loc*/}
+
+          </div> {/*flex-styling*/}
+      </div> {/*locations*/}
 
     </>
 
