ff1b96cd12075857781e2cb745baa7a5d1cd5f49
Katie Reider <ktreider5@gmail.com>
2022-07-12 12:05:20 -0400

    changes to navbar to make RSVP btn work properly

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,13 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-
-    const showButton = () => {
-        if(window.innerWidth <= 960){
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
-
-    useEffect(() => {
-        showButton();
-    }, [])
-
-    window.addEventListener('resize', showButton);
 
     return (
         <>
@@ -51,12 +36,9 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="" className='nav-links-mobile' onClick={closeMobileMenu}>
-                                RSVP
-                            </Link>
+                            <Button buttonStyle='btn--outline' className='nav-links-mobile' onClick={closeMobileMenu}>RSVP</Button>
                         </li>
                     </ul>
-                    {button && <Button buttonStyle='btn--outline'>RSVP</Button>}
                 </div>
             </nav>
         </>
